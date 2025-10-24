8e22a734f1bf88fd98d38ad13b7df875f4174fd3
mono0801 <austin0801@naver.com>
2025-10-24 20:18:19 +0900

    [Feature] Create CommentEntity

---
+++ src/main/java/com/example/boardserver/board/domain/Comment.java
@@ -0,0 +1,32 @@
+package com.example.boardserver.board.domain;
+
+import com.example.boardserver.common.domain.BaseEntity;
+import com.example.boardserver.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@DynamicInsert
+@DynamicUpdate
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "boardId", nullable = false)
+    private Board board;
+
+    private String comment;
+}
