622b0cf1591095582e2f358298da3e05c1b6eadb
mono0801 <austin0801@naver.com>
2025-10-25 18:59:29 +0900

    [Test] Test QueryDSL with BoardLikeRepository

--- src/main/java/com/example/boardserver/BoardServerApplication.java
+++ src/main/java/com/example/boardserver/BoardServerApplication.java
@@ -1,5 +1,7 @@
 package com.example.boardserver;
 
+import com.example.boardserver.board.domain.BoardLikeId;
+import com.example.boardserver.board.repository.BoardLikeRepositoryCustom;
 import com.example.boardserver.board.service.boardLikeService.boardLikeQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -13,18 +15,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @RequiredArgsConstructor
 public class BoardServerApplication {
 
-    private final boardLikeQueryService boardLikeQueryService;
+/*    private final BoardLikeQueryService boardLikeQueryService;
+    private final BoardLikeRepositoryCustom boardLikeRepositoryCustom;*/
 
     public static void main(String[] args) {
         SpringApplication.run(BoardServerApplication.class, args);
     }
 
-    @Bean
+/*    @Bean
     CommandLineRunner runAtStart() {
         return args -> {
             System.out.println("=== FindBoardLike 실행 ===");
             boardLikeQueryService.findBoardLike(1L, 1L);
+            boardLikeRepositoryCustom.findBoardLike(new BoardLikeId(1L, 1L)).ifPresent(System.out::println);
         };
-    }
+    }*/
 
 }
---
+++ src/main/java/com/example/boardserver/board/repository/BoardLikeRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.example.boardserver.board.repository;
+
+import com.example.boardserver.board.domain.BoardLikeId;
+import com.example.boardserver.board.domain.QBoard;
+import com.example.boardserver.board.domain.QBoardLike;
+import com.example.boardserver.board.dto.BoardLikeInfoDTO;
+import com.example.boardserver.user.domain.QUser;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardLikeRepositoryImpl implements BoardLikeRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+    private final QBoardLike boardLike = QBoardLike.boardLike ;
+    private final QBoard board = QBoard.board;
+    private final QUser user = QUser.user;
+
+    @Override
+    public Optional<BoardLikeInfoDTO> findBoardLike(BoardLikeId boardLikeId) {
+        BoardLikeInfoDTO result = queryFactory
+                .select(Projections.constructor(BoardLikeInfoDTO.class,
+                        user.userId,
+                        user.nickname,
+                        board.boardId,
+                        board.title))
+                .from(boardLike)
+                .join(boardLike.user, user)
+                .join(boardLike.board, board)
+                .where(
+                       // boardLike.boardLikeId.userId.eq(boardLikeId.getUserId()),
+                       // boardLike.boardLikeId.boardId.eq(boardLikeId.getBoardId())
+                        boardLike.boardLikeId.eq(boardLikeId)
+                )
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
+}
