f02cc442f00626dd1f65e6aecc570070a46d0845
mono0801 <austin0801@naver.com>
2025-10-27 22:19:25 +0900

    [Feature] Create Login Validation Logic

--- src/main/java/com/example/boardserver/user/repository/UserRepository.java
+++ src/main/java/com/example/boardserver/user/repository/UserRepository.java
@@ -3,7 +3,16 @@ package com.example.boardserver.user.repository;
 import com.example.boardserver.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    /**
+     * Email로 유저 정보를 조회하는 메소드
+     * @param email
+     * @return
+     */
+    Optional<User> findByEmail(String email);
 
     /**
      * 해당 Email 계정이 DB에 존재하는 지 검증하는 메소드
