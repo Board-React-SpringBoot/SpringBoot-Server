7fc40510c926e4d698da8011ba49be9e7c2b2cbf
mono0801 <austin0801@naver.com>
2025-10-28 14:10:31 +0900

    [Feature] Create Login API

---
+++ src/main/java/com/example/boardserver/user/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.boardserver.user.controller;
+
+import com.example.boardserver.auth.dto.CustomUserDetails;
+import com.example.boardserver.common.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
+@Tag(name = "User", description = "유저 관련 API")
+public class UserController {
+
+    @GetMapping("/test")
+    public ApiResponse<Map<String, Object>> test() {
+        CustomUserDetails user = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Long userId = user.getUserId();
+        String email = user.getUsername();
+        String password = user.getPassword();
+        String role = user.getAuthorities().iterator().next().getAuthority();
+        String nickname = user.getNickname();
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("userId", userId);
+        result.put("email", email);
+        result.put("password", password);
+        result.put("role", role);
+        result.put("nickname", nickname);
+
+        return ApiResponse.onSuccess(result);
+    }
+}
